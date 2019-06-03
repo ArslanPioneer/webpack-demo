85574e2986c06a3c78aaf449ee684b2ccf6a787a
rui.br <1041238415@qq.com>
2019-06-03 15:46:50 +0800

    HMR

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-console.log('123')
-import avatar from './icon-1@2x.jpg';
-import style from './index.scss';
-import './word.scss';
-import createImg from './createImg';
+// console.log('123')
+// import avatar from './icon-1@2x.jpg';
+// import style from './index.scss';
+// import './word.scss';
+// import createImg from './createImg';
+// import './style.css';
+import counter from './counter.js';
+import  number from './number.js';
 
-createImg();
-var img =new Image();
-img.src=avatar;
-img.classList.add(style.icon);
+counter()
+number()
+if(module.hot) {
+    module.hot.accept('./number',()=>{
+        document.body.removeChild(document.getElementById('number'));
+        number();
+    })
+}
+// createImg();
+// var img =new Image();
+// img.src=avatar;
+// img.classList.add(style.icon);
 
-var root=document.getElementById('root');
-var word =document.getElementById('word');
-console.log(123);
-console.log(345);
-word.innerHTML='<div class="iconfont icon-shouye"></div>'
-root.append(img);
+// var root=document.getElementById('root');
+// var word =document.getElementById('word');
+// console.log(123);
+// console.log(345);
+// var btn =document.createElement('button');
+// btn.innerHTML='新增';
+// document.body.appendChild(btn);
+// btn.onclick =function(){
+//     var div=document.createElement('div');
+//     div.innerHTML='item';
+//     document.body.appendChild(div);
+// }
+// word.innerHTML='<div class="iconfont icon-shouye"></div>'
+// root.append(img);
--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const {
+    CleanWebpackPlugin
+} = require('clean-webpack-plugin');
+const webpack =require('webpack');
 
 //plugin 可以在webpack运行到某个时刻的时候，帮助我做一些事情
 module.exports = {
     mode: 'development',
-    devtool:'cheap-module-eval-source-map',
+    devtool: 'cheap-module-eval-source-map',
     //production devtool:'cheap-module-source-map'
     //打包入口
     entry: './src/index.js',
     module: {
         rules: [{
-            test: /\.(jpg|png|gif)$/,
-            use: {
-                loader: 'url-loader',
-                options: {
-                    name: '[name].[ext]',
-                    outputPath: 'img/',
-                    limit: 1024
-                }
-            }
-        },
-        {
-            test: /\.(eot|ttf|svg|woff)$/,
-            use: {
-                loader: 'file-loader',
-            }
-        },
-        {
-            test: /\.scss$/,
-            use: ['style-loader',
-                {
-                    loader: 'css-loader',
+                test: /\.(jpg|png|gif)$/,
+                use: {
+                    loader: 'url-loader',
                     options: {
-                        importLoaders: 2,
-                        modules: true
+                        name: '[name].[ext]',
+                        outputPath: 'img/',
+                        limit: 1024
                     }
                 }
-                , 'sass-loader', 'postcss-loader']
-        }]
+            },
+            {
+                test: /\.(eot|ttf|svg|woff)$/,
+                use: {
+                    loader: 'file-loader',
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 2,
+                            modules: true
+                        }
+                    }, 'sass-loader', 'postcss-loader'
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader',
+                    {
+                        loader: 'css-loader',
+                    }, 'sass-loader', 'postcss-loader'
+                ]
+            }
+        ]
     },
-    devServer:{
+    devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: true,
-        open:true,
-        port: 9000
+        open: true,
+        port: 9000,
+        hot:true,
+        hotOnly:true
     },
     plugins: [
         //打包生成index.html文件
-        new HtmlWebpackPlugin({ template: 'src/index.html' }),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
         //By default, this plugin will remove all files inside webpack's output.path directory, as well as all unused webpack assets after every successful rebuild
         new CleanWebpackPlugin({
             // Simulate the removal of files
@@ -56,7 +72,9 @@ module.exports = {
             // Write Logs to Console
             dry: false,
             default: ['dist/*']
-        })
+        }),
+        //热加载，不刷新浏览器就可以更新页面
+        new webpack.HotModuleReplacementPlugin()
     ],
     //打包输出
     output: {
