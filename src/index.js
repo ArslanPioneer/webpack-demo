89d8dbe4d1a9dee94cbff5c782712da47083d9b5
Arslan <1041238415@qq.com>
2019-06-09 17:02:12 +0800

    清除无用文件，保持项目整洁

--- src/index.js
+++ src/index.js
@@ -1,4 +1,1 @@
-//Tree shaking 只支持module
-import {add } from './math.js';
-
-add(1,2)
+ import _  from 
--- src/test.js
+++
@@ -1,9 +0,0 @@
-// import './node_modules/.bin/babel'
-const arr =[
-    new Promise(()=>{}),
-    new Promise(()=>{}) 
- ]
- console.log(123)
- arr.map(item =>{
-     console.log(item);
- })
